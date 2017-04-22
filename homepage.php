212026b9dc1509cae5c3d52aae7413926ddeb4cf
Brandon Kelm <bkelm816@gmail.com>
2017-04-22 18:18:41 -0400

    dgfg

--- homepage.php
+++ homepage.php
@@ -88,7 +88,7 @@
                                     <br>
 
 
-                                    <h1>Welcome Back to Bank of DS <?php $row['first'] ?>!</h1>
+                                    <h1>Welcome Back to Bank of DS <?php echo $row['first'] " " $row['last'] ?>!</h1>
 
                                     <br> <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
 
