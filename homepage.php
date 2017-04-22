254407d1deb997546b6f40244f74a721c0ba7a15
Brandon Kelm <bkelm816@gmail.com>
2017-04-22 18:16:44 -0400

    sdfsdf

--- homepage.php
+++ homepage.php
@@ -29,8 +29,8 @@
     $queries = "SELECT first, last FROM DATABASE_USERS where userid='$user'";
     $result = queryMysql($queries);
     $row =  mysqli_fetch_assoc($result);
-    $first = $row["first"];
-    $last =  $row["last"];
+    //$first = $row["first"];
+    //$last =  $row["last"];
 
     ?>
 
@@ -88,7 +88,7 @@
                                     <br>
 
 
-                                    <h1>Welcome Back to Bank of DS <?php $first ?>!</h1>
+                                    <h1>Welcome Back to Bank of DS <?php $row['first'] ?>!</h1>
 
                                     <br> <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
 
